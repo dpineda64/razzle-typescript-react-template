e5d9ff154b2ce623ed1d77a42bb76e45b90cb17e
daniel <daniel64pineda@gmail.com>
2020-07-19 12:33:31 -0600

    fix: 🐛 testing

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(gql|graphql)$': 'jest-transform-graphql',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '\\.(|css|sass|scss|svg)$': '<rootDir>/src/test/__mocks__/svg-mock.ts',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@graphql(.*)$': '<rootDir>/src/graphql$1',
+    '^@queries(.*)$': '<rootDir>/src/graphql/queries$1',
+    '^@mutations(.*)$': '<rootDir>/src/graphql/mutations$1',
+    '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@routes(.*)$': '<rootDir>/src/routes$1',
+    '@svg(.*)$': '<rootDir>/src/svg$1',
+  },
+  snapshotSerializers: ['jest-emotion'],
+};
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,31 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
+import AccountQueries from '@queries/account-queries.gql';
+
+const mocks: MockedResponse[] = [
+  {
+    request: {
+      query: AccountQueries.Viewer,
+    },
+    result: {
+      data: {
+        viewer: {},
+      },
+    },
+  },
+];
 
 describe('<App />', () => {
   test('renders without exploding', () => {
     const div = document.createElement('div');
     ReactDOM.render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </MockedProvider>,
       div,
     );
   });
